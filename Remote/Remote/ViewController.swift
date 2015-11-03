b303d95e680f5549724bb773cb71701e81980675
ChenZhizhong <fdchzhzh@qq.com>
2015-11-03 22:09:58 +0800

    test

---
+++ Remote/Remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Remote
+//
+//  Created by 陈志中 on 15/11/3.
+//  Copyright © 2015年 陈志中. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
