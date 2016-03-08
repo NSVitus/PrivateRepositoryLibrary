645b448303ba711a89aee6ece4f86f4406584bbe
NSVitus <fdchzhzh@126.com>
2016-03-08 21:18:16 +0800

    new baidu map api test

---
+++ Demo/BaiduMapAPITestDemo/BaiduMapAPITestDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BaiduMapAPITestDemo
+//
+//  Created by 陈志中 on 16/3/8.
+//  Copyright © 2016年 陈志中. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
